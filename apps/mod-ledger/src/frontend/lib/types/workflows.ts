14d0c16744ec1ef73292e8ca05ec4d7d7b7c1cf2
psytor <psytor@gmail.com>
2025-07-26 07:38:22 +0200

    [RESET]

--- apps/mod-ledger/src/frontend/lib/types/workflows.ts
+++
@@ -1,9 +0,0 @@
-export interface Workflow {
-  name: string;
-  description: string;
-  [key: string]: any; // Allows for other properties like "dot_1-4", "dot_5", etc.
-}
-
-export interface Workflows {
-  [key: string]: Workflow;
-}
--- packages/database/index.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-// Export a single, shared Prisma client instance
-const prisma = new PrismaClient();
-export default prisma;
-
-// Export all the generated types from Prisma
-export * from '@prisma/client'
